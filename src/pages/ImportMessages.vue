4e1e103dc3963b26699cb1688e588fe89af0d249
Caio Eduardo Ragazzi Gemignani <ca.ragazzi@gmail.com>
2020-08-08 11:09:47 -0300

    Adicionado paginação e loading de importação e envio de mmensagens

--- src/pages/ImportMessages.vue
+++ src/pages/ImportMessages.vue
@@ -1,67 +1,93 @@
 <template>
-  <b-container>
-    <b-form class="form" @submit.stop.prevent="submmit">
-      <!-- Styled -->
-      <div class="form-header pb-3">
-        <b-form-file
-          class="w-50"
-          v-model="file"
-          :state="Boolean(file)"
-          placeholder="Escolha um arquivo ou arraste aqui..."
-          browse-text="Procurar"
-          drop-placeholder="Arraste um arquivo aqui..."
-        ></b-form-file>
-        <div>
-          <b-button variant="primary" @click="sheetToJson">Importar</b-button>
-          <b-button class="ml-2" variant="danger" @click="clearAll">Limpar</b-button>
-          <!-- <div class="mt-3">Selected file: {{ file ? file.name : '' }}</div> -->
-        </div>
-      </div>
-      <div>
-        <b-form-textarea
-          id="textarea"
-          v-model="message"
-          placeholder="Digite a mensagem aqui..."
-          rows="3"
-          max-rows="6"
-        ></b-form-textarea>
-      </div>
-      <div class="form-bottom pt-3">
-        <div>
-          <b-button
-            class="mr-2"
-            v-for="(column, idx) in columns"
-            :key="idx"
-            @click="addCustomMessage(column)"
-          >{{ column }}</b-button>
+  <b-overlay :show="isLoading" rounded="sm">
+    <b-container class="pt-5">
+      <b-form class="form" @submit.stop.prevent="submmit">
+        <!-- Styled -->
+        <div class="form-header pb-3">
+          <b-form-file
+            class="w-50"
+            v-model="file"
+            :state="Boolean(file)"
+            placeholder="Escolha um arquivo ou arraste aqui..."
+            browse-text="Procurar"
+            drop-placeholder="Arraste um arquivo aqui..."
+          ></b-form-file>
+          <div>
+            <!-- <b-spinner label="Spinning" v-if="isLoading"></b-spinner> -->
+            <b-button variant="primary" @click="sheetToJson">Importar</b-button>
+            <b-button class="ml-2" variant="danger" @click="clearAll">Limpar</b-button>
+          </div>
         </div>
         <div>
-          <b-button type="submit" variant="primary">
-            <b-spinner v-if="isLoading" label="Spinning"></b-spinner>
-            <span v-else>Enviar</span>
-          </b-button>
+          <b-form-textarea
+            id="textarea"
+            v-model="message"
+            placeholder="Digite a mensagem aqui..."
+            rows="3"
+            max-rows="6"
+          ></b-form-textarea>
         </div>
+        <div class="form-bottom pt-3">
+          <div>
+            <b-button
+              class="mr-2"
+              v-for="(column, idx) in columns"
+              :key="idx"
+              @click="addCustomMessage(column)"
+            >{{ column }}</b-button>
+          </div>
+          <div>
+            <b-button type="submit" variant="primary">
+              <!-- <b-spinner v-if="isLoading" label="Spinning"></b-spinner> -->
+              <span>Enviar</span>
+            </b-button>
+          </div>
+        </div>
+        <div class="pt-3">
+          <b-table
+            striped
+            hover
+            :per-page="paginationPerPage"
+            :current-page="paginationCurrentPage"
+            :items="sheetItems"
+            id="table"
+          />
+          <b-pagination
+            v-if="paginationRows > 0"
+            v-model="paginationCurrentPage"
+            :total-rows="paginationRows"
+            :per-page="paginationPerPage"
+            aria-controls="table"
+          />
+        </div>
+        <div class="footer pt-3">
+          <p>
+            Desenvolvido por
+            <a href>Samuel Evangelista</a>
+          </p>
+        </div>
+      </b-form>
+    </b-container>
+    <template v-slot:overlay>
+      <div class="d-flex justify-content-center flex-column">
+        <atom-spinner class="align-self-center" :animation-duration="1000" :size="100" :color="'#000000'" />
+        <b>{{ loadingMessage }}...</b>
       </div>
-      <div class="pt-3">
-        <b-table striped hover :items="sheetItems"></b-table>
-      </div>
-      <div class="footer pt-3">
-        <p>
-          Desenvolvido por
-          <a href>Samuel Evangelista</a>
-        </p>
-      </div>
-    </b-form>
-  </b-container>
+    </template>
+  </b-overlay>
 </template>
 
 <script>
 import XLSX from "xlsx";
 import axios from "axios";
+import { AtomSpinner  } from "epic-spinners";
 
 export default {
   name: "ImportMessages",
   props: {},
+  components: {
+    AtomSpinner,
+  },
   data() {
     return {
       file: null,
@@ -70,7 +96,15 @@ export default {
       message: "",
       sheetItems: [],
       isLoading: false,
+      loadingMessage: "",
+      paginationCurrentPage: 1,
+      paginationPerPage: 10,
     };
+  },
+  computed: {
+    paginationRows() {
+      return this.sheetItems.length;
+    },
   },
   methods: {
     sheetToJson() {
@@ -82,6 +116,8 @@ export default {
         this.showAlert("Extensão não permitida.", "warning");
         return;
       }
+      this.isLoading = true;
+      this.loadingMessage = "Importando arquivo";
       this.reader.onload = (e) => {
         var data = e.target.result;
         data = new Uint8Array(data);
@@ -96,6 +132,7 @@ export default {
         this.sheetItems = result;
         this.sheetItems = result[Object.keys(result)[0]];
         this.formatNumbers(workbook.SheetNames, result);
+        this.isLoading = false;
       };
 
       this.reader.readAsArrayBuffer(this.file);
@@ -106,6 +143,7 @@ export default {
         return;
       }
       this.isLoading = true;
+      this.loadingMessage = "Enviando mensagens";
       axios
         .post("http://localhost:3000/messages/send", {
           columnSheet: this.sheetItems,
@@ -136,6 +174,7 @@ export default {
       this.columns = [];
       this.sheetItems = [];
       this.message = "";
+      this.paginationCurrentPage = 1;
     },
     showAlert(message, status) {
       this.$bvToast.toast(message, {
