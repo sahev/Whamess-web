abba85ae6245ce8d291d6db33b877654c733f9d2
sahev <s4mus.sh@gmail.com>
2020-09-09 22:35:13 -0300

    Atualização da tela de usuário

--- src/pages/account.vue
+++ src/pages/account.vue
@@ -1,6 +1,5 @@
 <template>
   <b-overlay :show="isLoading" rounded="sm">
-    <b-container>
       <div>
         <b-navbar type="dark" variant="dark">
           <b-navbar-nav>
@@ -11,26 +10,19 @@
           <b-collapse id="navbar-toggle-collapse" is-nav>
             <b-navbar-nav class="ml-auto">
               <b-nav-item-dropdown :text="this.profile.name + ' ' + this.profile.lastname" right>
-                <b-dropdown-item>Account</b-dropdown-item>
-                <b-dropdown-item href="#">Settings</b-dropdown-item>
+                <b-dropdown-item link to="/home">Home</b-dropdown-item>
                 <b-dropdown-item @click="signout">Logout</b-dropdown-item>
               </b-nav-item-dropdown>
             </b-navbar-nav>
           </b-collapse>
         </b-navbar>
-        <div class="mr-auto">
-          <ul>Nome: {{profile.name}} {{profile.lastname}}</ul>
-          <ul>E-mail: {{profile.email}}</ul>
-          <ul>Telefone: {{profile.phone}}</ul>
-          <ul>Mensagens disponíveis: {{2000 - profile.messagesCount}}</ul>
-          <ul>Criado em: {{profile.createdOn}}</ul>
-          <ul>Data de vencimento: {{profile.expiresOn}}</ul>
-        </div>
-        <div>
-          <b-card bg-variant="light">
+      </div>
+      <div>
+        <b-card bg-variant="light">
+          <b-form @submit="updateProfile" class="m-4">
             <b-form-group
               label-cols-lg="3"
-              label="Shipping Address"
+              label="Perfil:"
               label-size="lg"
               label-class="font-weight-bold pt-0"
               class="mb-0"
@@ -41,7 +33,27 @@
                 label-align-sm="right"
                 label-for="inp-name"
               >
-                <b-form-input id="inp-name"></b-form-input>
+                <b-form-input
+                  v-model="profile.name"
+                  :value="profile.name"
+                  id="inp-name"
+                  :disabled="!this.updatebutton"
+                ></b-form-input>
+              </b-form-group>
+
+              <b-form-group
+                label-cols-sm="3"
+                label="Sobrenome:"
+                label-align-sm="right"
+                label-for="inp-lastname"
+                :disabled="!this.updatebutton"
+              >
+                <b-form-input
+                  v-model="profile.lastname"
+                  :value="profile.lastname"
+                  id="inp-lastname"
+                  :disabled="!this.updatebutton"
+                ></b-form-input>
               </b-form-group>
 
               <b-form-group
@@ -50,7 +62,12 @@
                 label-align-sm="right"
                 label-for="inp-email"
               >
-                <b-form-input id="inp-email"></b-form-input>
+                <b-form-input
+                  v-model="profile.email"
+                  :value="profile.email"
+                  id="inp-email"
+                  :disabled="!this.updatebutton"
+                ></b-form-input>
               </b-form-group>
 
               <b-form-group
@@ -59,7 +76,12 @@
                 label-align-sm="right"
                 label-for="inp-phone"
               >
-                <b-form-input id="inp-phone"></b-form-input>
+                <b-form-input
+                  v-model="profile.phone"
+                  :value="profile.phone"
+                  id="inp-phone"
+                  :disabled="!this.updatebutton"
+                ></b-form-input>
               </b-form-group>
 
               <b-form-group
@@ -68,7 +90,12 @@
                 label-align-sm="right"
                 label-for="inp-createdon"
               >
-                <b-form-input id="inp-createdon"></b-form-input>
+                <b-form-input
+                  v-model="profile.createdOn"
+                  :value="profile.createdOn"
+                  id="inp-createdon"
+                  disabled
+                ></b-form-input>
               </b-form-group>
 
               <b-form-group
@@ -77,20 +104,55 @@
                 label-align-sm="right"
                 label-for="inp-expiration"
               >
-                <b-form-input id="inp-expiration"></b-form-input>
-              </b-form-group>              
+                <b-form-input
+                  v-model="profile.expiresOn"
+                  :value="profile.expiresOn"
+                  id="inp-expiration"
+                  disabled
+                ></b-form-input>
+              </b-form-group>
 
+              <div class="float-right">
+                <b-button-toolbar key-nav>
+                  <div id="submit" class="mx-1">
+                    <b-button
+                      v-if="!updatebutton"
+                      @click="update"
+                      variant="primary"
+                      style="width: 15rem"
+                    >Atualizar</b-button>
+                  </div>
+
+                  <div id="cancel" class="mx-1">
+                    <b-button
+                      v-show="updatebutton"
+                      type="submit"
+                      variant="success"
+                      style="width: 15rem"
+                    >Salvar</b-button>
+                  </div>
+
+                  <div id="cancel" class="mx-1">
+                    <b-button
+                      v-show="updatebutton"
+                      @click="update"
+                      variant="outline-danger"
+                      style="width: 15rem"
+                    >Cancelar</b-button>
+                  </div>
+                </b-button-toolbar>
+              </div>
             </b-form-group>
-          </b-card>
-        </div>
+          </b-form>
+        </b-card>
       </div>
+
       <div class="footer pt-3">
         <p>
           Desenvolvido por
           <a href>Samuel Evangelista</a>
         </p>
       </div>
-    </b-container>
   </b-overlay>
 </template>
 <script>
@@ -105,6 +167,7 @@ export default {
       isLoading: false,
       islogged: false,
       profile: {},
+      updatebutton: false,
     };
   },
   created() {
@@ -112,6 +175,9 @@ export default {
   },
   computed: {},
   methods: {
+    update() {
+      this.updatebutton = !this.updatebutton;
+    },
     async signout() {
       localStorage.removeItem("token");
       router.push("/");
@@ -124,9 +190,28 @@ export default {
       let res = await axios.get(`users/getbyemail/${email}`, {
         headers: { Authorization: "Bearer " + token },
       });
-
       this.profile = res.data;
-      console.log(this.profile);
+      this.profile.createdOn = this.profile.createdOn.split("T")[0];
+      this.profile.expiresOn = this.profile.expiresOn.split("T")[0];
+    },
+    async updateProfile() {
+      try {
+        const token = localStorage.getItem("token");
+        var jwt = require("jsonwebtoken");
+        var { email } = jwt.verify(token, "secretKey");
+
+        let res = await axios.patch(`users/${email}`, this.profile, {
+          headers: { Authorization: "Bearer " + token },
+        });
+        this.updatebutton = !this.updatebutton;
+        this.profile = res.data;
+        this.profile.createdOn = this.profile.createdOn.split("T")[0];
+        this.profile.expiresOn = this.profile.expiresOn.split("T")[0];
+      } catch {
+        alert("sessao expirada");
+        localStorage.removeItem("token");
+        router.push("/");
+      }
     },
   },
 };
